ccb87079baeb371206056725db3dd4c2b0bcf9a7
ert-developer <ert.developer@gmail.com>
2024-10-03 13:29:58 +0530

    Location denied and success done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet, Image, Platform } from "react-native";
+import { StyleSheet, Image, Platform, View, Text } from "react-native";
 
 import { Collapsible } from "@/components/Collapsible";
 import { ExternalLink } from "@/components/ExternalLink";
@@ -7,10 +7,38 @@ import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import TabLayout from "./_layout";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 export default function HomeScreen() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      console.log("status", status);
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
   return (
-    <TabLayout />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>{errorMsg}</Text>
+      <TabLayout />
+    </View>
   );
 }
 
--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,16 +1,61 @@
 import * as React from "react";
-import { Button, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Button,
+  Linking,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import BottomSheet from "@/components/bottom";
 import { moderateScale } from "react-native-size-matters";
+import * as Location from "expo-location";
 
 export function MenuScreen() {
   const navigation = useNavigation();
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
 
   const [open, setOpen] = React.useState(false);
 
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      console.log("status", status);
+      setErrorMsg(status);
+
+      if (status !== "granted") {
+        // Show the alert if permission is denied
+        Alert.alert(
+          "Permission Required",
+          "Location permission is required. Please enable it in settings.",
+          [
+            {
+              text: "OK",
+              onPress: () => Linking.openSettings(),
+            },
+            {
+              text: "Cancel",
+              style: "cancel",
+            },
+          ]
+        );
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {errorMsg === Location.PermissionStatus.DENIED && errorMsg ? (
+        <Button title={"Grant Permission"} onPress={Linking.openSettings} />
+      ) : (
+        <Text>{"Accepted"}</Text>
+      )}
       <Text>Menu Screen</Text>
       <Button title="Click" onPress={() => setOpen(true)} />
       <BottomSheet
